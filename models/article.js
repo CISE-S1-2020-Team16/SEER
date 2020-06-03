af48270c59488024f11b32465234d14c0d2082d9
Aizeru <53991455+Aizeru@users.noreply.github.com>
2020-06-03 15:00:38 +1200

    Update article.js

--- models/article.js
+++ models/article.js
@@ -2,17 +2,47 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Article = new Schema(
-    {
-        article: {type: String, required: true}, 
-        author: {type: String, required: true}, 
-        title: {type: String, required: true}, 
-        journal: {type: String, required: true}, 
-        year: {type: String, required: true},  
-        volume: {type: String, required: true}, 
-        number: {type: String, required: true}, 
-        pages: {type: String, required: true}, 
-        month: {type: String, required: true},
-    },
+    {	
+	
+		articleinfo: {
+        article: String ,
+        author: String ,
+        title: String ,
+        journal: String ,
+        year: Number ,
+        volume: Number ,
+        number: Number ,
+        pages: [Number],
+        month: Number, 
+		sourcetype: [ 'article', 'book','booklet','conference','inbook','incollection','inprocceedings','manual','masterthesis','misc','phdthesis','proceedings','techreport','unpublished'] ,
+		rating : Number
+		} , 
+		
+		
+        researchdesign:
+        {
+		question: String,
+		metric: String,
+		researchparticipants: ['undergraduate','postgraduate','practitioners'],
+		researchmethod: ['case study','field-observation','experiment','interview','survey']
+		},
+		
+		evidenceitem: {
+		outcome: ['yes','no','sometimes'],
+		semethodologies: ['scrum','waterfall','spiral','xp','rational','unified proccess','crystal','clean room','feature driven development',
+		'model driven development','domain driven development','formal methods','problem driven development','cloud computing','service oriented development',
+		'valuse driven development','product driven development','agile'],
+		semethod: ['tdd','bdd','pair programming','planning poker','daily standup','story boards','user story mapping','continous','intergration','retrospectives','burndown charts','requirements','requirements prioritisation','version control', 'code sharing']
+		} ,
+	
+		statusitem:{
+		statustype: ['to be moderated','accepted','rejected','analysis accepted'],
+		date: { type: Date, default: Date.now },
+		username: String
+		}
+		
+		
+    }
 )
 
 module.exports = mongoose.model('articles', Article)
