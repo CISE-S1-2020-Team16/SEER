87ff51bb1674e277343672cc3e586e160fa71ae5
Aizeru <53991455+Aizeru@users.noreply.github.com>
2020-06-11 13:00:14 +1200

    Update ArticlesInsert.jsx

--- client/src/pages/ArticlesInsert.jsx
+++ client/src/pages/ArticlesInsert.jsx
@@ -16,50 +16,7 @@ const Wrapper = styled.div.attrs({
 const Label = styled.label`
     margin: 5px;
 `
-const Main = styled("div")`
-  font-family: sans-serif;
-  background: #f0f0f0;
-  height: 100vh;
-`
 
-const DropDownContainer = styled("div")`
-  width: 10.5em;
-  margin: 0 auto;
-`
-
-const DropDownHeader = styled("div")`
-  margin-bottom: 0.8em;
-  padding: 0.4em 2em 0.4em 1em;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
-  font-weight: 500;
-  font-size: 1.3rem;
-  color: #3faffa;
-  background: #ffffff;
-`
-
-const DropDownListContainer = styled("div")``
-
-const DropDownList = styled("ul")`
-  padding: 0;
-  margin: 0;
-  padding-left: 1em;
-  background: #ffffff;
-  border: 2px solid #e5e5e5;
-  box-sizing: border-box;
-  color: #3faffa;
-  font-size: 1.3rem;
-  font-weight: 500;
-  &:first-child {
-    padding-top: 0.8em;
-  }
-`
-
-const ListItem = styled("li")`
-  list-style: none;
-  margin-bottom: 0.8em;
-`
-
-const options = ["Mangoes", "Apples", "Oranges"];
 
 const InputText = styled.input.attrs({
     className: 'form-control',
@@ -238,16 +195,7 @@ handleChangeInputConfidenceRating = async event => {
 
     render() {
         const {article, author, title, journal, year, volume, number, pages, month, sourcetype, credibilityrating, confidencerating, requestion, metric, researchparticipants, researchmethod, outcome, semethodologies, semethod,statustype} = this.state
-        const [isOpen, setIsOpen] = useState(false);
-        const [selectedOption, setSelectedOption] = useState(null);
-      
-        const toggling = () => setIsOpen(!isOpen);
-      
-        const onOptionClicked = value => () => {
-          setSelectedOption(value);
-          setIsOpen(false);
-          console.log(selectedOption);
-        };
+
         return (
             <Wrapper>
                 <Title>Create Article</Title>
