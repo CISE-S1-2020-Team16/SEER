3af8a847f9d3a459a7fa5fd4dd0d3311d4ee4a04
Finn S <finnstainton@gmail.com>
2020-06-12 13:53:16 +1200

    Merged from develop

---
+++ client/src/pages/ArticlesInsert.test.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { act } from "react-dom/test-utils";
+
+let rootContainer;
+
+beforeEach(() => {
+  rootContainer = document.createElement("div");
+  document.body.appendChild(rootContainer);
+});
+
+afterEach(() => {
+  document.body.removeChild(rootContainer);
+  rootContainer = null;
+});
+
+describe("", () => {
+  it("Renders Article Title", () => {
+    act(() => {
+      ReactDOM.render(<Label />, rootContainer);
+    });
+    const h1 = rootContainer.querySelector("h1");
+    expect(h1.textContent).to.equal("Hello World");
+  });
+});
